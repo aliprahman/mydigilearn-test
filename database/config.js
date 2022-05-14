4c91989bb5718ea3c59e457689ff5cfc548bae0e
alip rahman <aliprrahman@gmail.com>
2022-05-14 14:57:27 +0700

    update config db

--- database/config.js
+++ database/config.js
@@ -10,5 +10,11 @@ module.exports = {
   dialect: 'postgres',
   dialectOptions: {
     bigNumberStrings: true
-  }
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
 };
