d09f366d72afad2f35209c945607f582e074d059
alip rahman <aliprrahman@gmail.com>
2022-05-14 14:17:33 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 // setup config app
 app.use(bodyParser.urlencoded({ extended: true }));
